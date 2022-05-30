85dfa787fe5fed5add092b92706adabd685e46a1
Nyabonyo <nyawangasyprose@gmail.com>
2022-05-30 17:17:32 +0300

    my commit

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
 number = 98
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-addition = Battery street
-print(number + addition)
+print(str(number) + " Battery street")
 print("\n")
