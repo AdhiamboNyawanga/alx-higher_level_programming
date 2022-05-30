d5a312c6aa4cd99929f3eede7aad6c21f0866bde
Nyabonyo <nyawangasyprose@gmail.com>
2022-05-30 20:44:21 +0300

    my commits

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print( f"{str} {str}  {str} \n")
-print(f"{str[0:9]} \n")
+print("{:s}{:s}{:s}".format(str, str, str))
+print("{:.9s}".format(str))
