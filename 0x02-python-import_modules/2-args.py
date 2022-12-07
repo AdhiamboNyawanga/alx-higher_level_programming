22f9e7174c0dbc53cf6f858a3efc6d9504c55b91
Nyabonyo6 <nyawangasyprose@gmail.com>
2022-12-07 12:28:49 +0300

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
-
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+    from sys import argv
+    res = ""
+    argc = len(argv) - 1
+    delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
+    for count, arg in enumerate(argv[1:]):
+        res += '{:d}: {}\n'.format(count+1, arg)
+    print('{:d} argument{}\n{}'.format(argc, delim, res), end="")
