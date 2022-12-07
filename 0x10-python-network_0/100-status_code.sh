a66290531ee24bab85e48287134f775c2b317582
Nyabonyo6 <nyawangasyprose@gmail.com>
2022-12-07 13:34:05 +0300

    my commit

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  script that sends a request to a URL passed as an argument
-curl -so /dev/null -w "%{http_code}" "$1"
+# script that sends a request to a URL passed as an argument
+curl -o /dev/null -sw "%{http_code}" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Pass two varibles in post method
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
